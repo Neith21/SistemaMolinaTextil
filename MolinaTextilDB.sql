27aba0fc6dafc6f1ff02721a3abe823d60ee95e3
Josue-Montoya <u20210157@univo.edu.sv>
2024-03-23 11:57:31 -0600

    Modified by Josue

--- MolinaTextilDB.sql
+++ MolinaTextilDB.sql
@@ -15,22 +15,14 @@ CREATE TABLE InventoryRawMaterials(
 );
 GO
 
--- Datos del cliente
+-- Datos del pedido del cliente
 CREATE TABLE CustomerOrder(
 	CustomerOrderID INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
 	CustomerName NVARCHAR(50) NOT NULL,
-	TotalAmount MONEY NOT NULL
-);
-GO
-
--- Detalles de la orden del cliente
-CREATE TABLE CustomerOrderDetails(
-	CustomerOrderDetailsID INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-	CustomerOrderID INT FOREIGN KEY REFERENCES CustomerOrder(CustomerOrderID)  NOT NULL,
 	CustomerOrderIssue NVARCHAR(50) NOT NULL,
 	CustomerOrderDescription NVARCHAR(255) NOT NULL,
-	TextileRecipe NVARCHAR(255),
-	Amount MONEY NOT NULL
+	TextileRecipe NVARCHAR(255) NOT NULL,
+	TotalAmount MONEY NOT NULL
 );
 GO
 
@@ -46,28 +38,21 @@ CREATE TABLE Employee (
 GO
 -- Agregar datos a la tabla InventoryRawMaterials
 INSERT INTO InventoryRawMaterials (RawMaterialName, RawMaterialDescription, SupplierContact, StockQuantity, StockQuantityUsed)
-VALUES ('Cotton', 'High-quality cotton material', 'Supplier1@gmail.com', 1000, 0),
-       ('Polyester', 'Polyester fabric for various uses', 'Supplier2@gmail.com', 800, 0),
-       ('Wool', 'Fine wool material for luxury products', 'Supplier3@gmail.com', 500, 0);
-GO
+VALUES 
+    ('Cotton', 'High-quality cotton material', 'supplier1@gmail.com', 1000, 0),
+    ('Polyester', 'Polyester fabric for various uses', 'supplier2@gmail.com', 800, 0),
+    ('Wool', 'Fine wool material for luxury products', 'supplier3@gmail.com', 500, 0);
+
 -- Agregar datos a la tabla CustomerOrder
-INSERT INTO CustomerOrder (CustomerName, TotalAmount)
-VALUES ('John Doe', 500),
-       ('Jane Smith', 700),
-       ('Alice Johnson', 900);
-GO
--- Agregar datos a la tabla CustomerOrderDetails
-INSERT INTO CustomerOrderDetails (CustomerOrderID, CustomerOrderIssue, CustomerOrderDescription, TextileRecipe, Amount)
-VALUES (1, 'Delayed delivery', 'Ordered cotton fabric for shirts', 'Cotton 100%', 200),
-       (2, 'Incorrect color', 'Ordered polyester fabric for dresses', 'Polyester 80%, Spandex 20%', 300),
-       (3, 'Defective material', 'Ordered wool fabric for sweaters', '100% Pure Wool', 400);
-GO
+INSERT INTO CustomerOrder (CustomerName, CustomerOrderIssue, CustomerOrderDescription, TextileRecipe, TotalAmount)
+VALUES 
+    ('John Doe', 'None', 'Ordered cotton fabric for shirts', 'Cotton 100%', 200),
+    ('Jane Smith', 'Delayed delivery', 'Ordered polyester fabric for dresses', 'Polyester 80%, Spandex 20%', 300),
+    ('Alice Johnson', 'Defective material', 'Ordered wool fabric for sweaters', '100% Pure Wool', 400);
+
 -- Agregar datos a la tabla Employee
 INSERT INTO Employee (EmployeeName, EmployeeContact, EmployeeUsername, EmployeePassword, EmployeeRoleID)
-VALUES ('Admin User', 'admin@gmail.com', 'admin', 'adminpassword', 1),
-       ('John Smith', 'john@gmail.com', 'john', 'johnpassword', 2),
-       ('Emily Johnson', 'emily@gmail.com', 'emily', 'emilypassword', 2);
-GO
-Update Employee set EmployeeName = 'John Cena', EmployeeContact = 'cena@gmail.com' where EmployeeID = 1
-Go
-Select * From Employee
+VALUES 
+    ('Admin User', 'admin@gmail.com', 'admin', 'adminpassword', 1),
+    ('John Smith', 'john@gmail.com.com', 'john', 'johnpassword', 2),
+    ('Emily Johnson', 'emily@gmail.com', 'emily', 'emilypassword', 2);
