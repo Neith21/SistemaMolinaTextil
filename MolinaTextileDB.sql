58063a07dae33a8e665b2418e28fd8a7fe4d743b
Neith21 <cristian.1945theend@gmail.com>
2024-04-13 15:26:51 -0600

    Subiendo avance

--- MolinaTextileDB.sql
+++ MolinaTextileDB.sql
@@ -1,105 +1,192 @@
-CREATE DATABASE MolinaTextileDB
+CREATE DATABASE MolinaTextileDB;
 GO
 
-USE MolinaTextileDB
+USE MolinaTextileDB;
 GO
 
 CREATE TABLE Employees(
-	EmployeeID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameEmployee NVARCHAR(50),
-	LastnameEmployee NVARCHAR(50),
-	ContactEmployee NVARCHAR(50)
-)
+    EmployeeId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    EmployeeName NVARCHAR(50) NOT NULL,
+    EmployeeLastname NVARCHAR(50) NOT NULL,
+    EmployeeAddress VARCHAR(255) NOT NULL,
+    EmployeePhone VARCHAR(30) NOT NULL,
+    EmployeeEmail NVARCHAR(100) NOT NULL
+);
 GO
 
-CREATE TABLE CredentialsLogin(
-	CredentialID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	EmployeeID INT FOREIGN KEY REFERENCES Employees(EmployeeID),
-	UsernameCredential NVARCHAR(20),
-	PasswordCredential NVARCHAR(255)
-)
+INSERT INTO Employees VALUES('Juan', 'Perez', 'Colina Abajo', '1111-1111', 'jpca12@gmail.com');
+GO
+
+CREATE TABLE LoginCredentials(
+    LoginCredentialId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    Username NVARCHAR(20) NOT NULL,
+    Password NVARCHAR(255) NOT NULL,
+    EmployeeId INT NOT NULL FOREIGN KEY REFERENCES Employees(EmployeeId) ON DELETE CASCADE
+);
 GO
 ---------------------------------------------------------------------
 CREATE TABLE Suppliers(
-	SupplierID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameSupplier NVARCHAR(50),
-	ManagerSupplier NVARCHAR(50),
-	ContactSupplier NVARCHAR(50)
-)
+	SupplierId INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	SupplierName NVARCHAR(50) NOT NULL,
+	Manager NVARCHAR(50),
+	SupplierPhone VARCHAR(30) NOT NULL,
+	SupplierEmail NVARCHAR(100)
+);
 GO
 
 CREATE TABLE Categories(
-	CategoryID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameCategory NVARCHAR(50),
-	DescriptionCategory NVARCHAR(255)
-)
+	CategoryId INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	CategoryName VARCHAR(50) NOT NULL,
+	CategoryDescription VARCHAR(255)
+);
 GO
 
-CREATE TABLE Materials(
-	MaterialID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameMaterial NVARCHAR(50),
-	DescriptionMaterial NVARCHAR(255),
-	CategoryID INT FOREIGN KEY REFERENCES Categories(CategoryID),
-	SupplierID INT FOREIGN KEY REFERENCES Suppliers(SupplierID),
-	purchasePriceMaterial MONEY,
-	QuantityMaterial DECIMAL
-)
-GO
-
-CREATE TABLE DetailsPatterns(
-	DetailPatternID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	MaterialID INT FOREIGN KEY REFERENCES Materials(MaterialID),
-	Quantity DECIMAL
-)
+CREATE TABLE RawMaterials(
+	RawMaterialId INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	RawMaterialName VARCHAR(50) NOT NULL,
+	RawMaterialDescription VARCHAR(255),
+	RawMaterialPurchasePrice MONEY,
+	RawMaterialQuantity DECIMAL NOT NULL,
+	CategoryId INT FOREIGN KEY REFERENCES Categories(CategoryId),
+	SupplierId INT NOT NULL FOREIGN KEY REFERENCES Suppliers(SupplierId),
+);
 GO
 
 CREATE TABLE Patterns(
-	PatternID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NamePattern NVARCHAR(50),
-	DescriptionPattern NVARCHAR(255),
-	DetailPatternID INT FOREIGN KEY REFERENCES DetailsPatterns(DetailPatternID)
-)
+	PatternId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	PatternName NVARCHAR(50) NOT NULL,
+	PatternDescription VARCHAR(255)
+);
+GO
+
+CREATE TABLE PatternDetails(
+	PatternDetailId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	RawMaterialQuantity DECIMAL NOT NULL,
+	MaterialId INT NOT NULL FOREIGN KEY REFERENCES RawMaterials(RawMaterialId),
+	PatternId INT FOREIGN KEY REFERENCES Patterns(PatternId)
+);
 GO
 
 CREATE TABLE States(
-	StateID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameState NVARCHAR(50),
-	DescriptionState NVARCHAR(255)
-)
+	StateId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	StateName VARCHAR(30) NOT NULL,
+	StateDescription VARCHAR(255)
+);
 GO
 
 CREATE TABLE Products(
-	ProductID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameProduct NVARCHAR(50),
-	SizeProduct NVARCHAR(10),
-	PatternID INT FOREIGN KEY REFERENCES Materials(MaterialID),
-	StateID INT FOREIGN KEY REFERENCES States(StateID)
-)
+	ProductId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	ProductName VARCHAR(50) NOT NULL,
+	ProductSize VARCHAR(10),
+	PatternId INT NOT NULL FOREIGN KEY REFERENCES Patterns(PatternId),
+	StateId INT NOT NULL FOREIGN KEY REFERENCES States(StateId)
+);
 GO
 
 CREATE TABLE Customers(
-	CustomerID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	NameCustomer NVARCHAR(50),
-	AddressCustomer NVARCHAR(50),
-	phoneCustomer NVARCHAR(20)
-)
+	CustomerId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	CustomerName NVARCHAR(100) NOT NULL,
+	CustomerAddress NVARCHAR(255),
+	CustomerPhone VARCHAR(20)
+);
 GO
 
-CREATE TABLE CustomersOrders(
-	CustomerOrderID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID),
-	EmployeeID INT FOREIGN KEY REFERENCES Employees(EmployeeID),
-	CreationDate NVARCHAR(50),
-	DeliveryDate NVARCHAR(50),
-	OrderPrice MONEY,
-	StateID INT FOREIGN KEY REFERENCES States(StateID)
-)
+CREATE TABLE CustomerOrders(
+	CustomerOrderId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	CreationDate DATE NOT NULL,
+	DeliveryDate DATE,
+	TotalAmount MONEY NOT NULL,
+	CustomerId INT NOT NULL FOREIGN KEY REFERENCES Customers(CustomerId),
+	EmployeeId INT NOT NULL FOREIGN KEY REFERENCES Employees(EmployeeId),
+	StateId INT NOT NULL FOREIGN KEY REFERENCES States(StateId)
+);
+GO
 
-CREATE TABLE CustomersOrdersDetails(
-	CustomerOrderDetailID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	CustomerOrderID INT FOREIGN KEY REFERENCES CustomersOrders(CustomerOrderID),
-	ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
-	UnitPrice MONEY,
-	Amount INT
+CREATE TABLE CustomerOrderDetails(
+	CustomerOrderDetailId INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	UnitPrice MONEY NOT NULL,
+	CustomerOrderDetailQuantity INT NOT NULL,
+	CustomerOrderId INT NOT NULL FOREIGN KEY REFERENCES CustomerOrders(CustomerOrderId),
+	ProductId INT NOT NULL FOREIGN KEY REFERENCES Products(ProductId)
+);
+GO
+
+--SP Credentials--
+CREATE OR ALTER PROCEDURE spLoginCredentials_Delete
+	@DetailId INT
+AS
+BEGIN
+	DELETE FROM LoanDetails
+	WHERE DetailId = @DetailId;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE spLoginCredentials_GetAll
+AS
+BEGIN
+	SELECT ld.DetailId, ld.Quantity, ld.LoanId, b.Title
+	FROM LoanDetails ld
+	INNER JOIN Books b
+	ON ld.BookId = b.BookId;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE spLoginCredentials_GetAllSpecific
+	@LoanId INT
+AS
+BEGIN
+	SELECT ld.DetailId, ld.Quantity, ld.LoanId, b.Title
+	FROM LoanDetails ld
+	INNER JOIN Books b
+	ON ld.BookId = b.BookId
+	WHERE ld.LoanId = @LoanId;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE spLoginCredentials_GetById
+	@DetailId INT
+AS
+BEGIN
+	 SELECT DetailId, LoanId, BookId, Quantity
+	 FROM LoanDetails
+	 WHERE DetailId = @DetailId;
+END
+GO
+
+CREATE OR ALTER PROCEDURE spLoginCredentials_Insert
+(
+	@Quantity INT,
+    @LoanId INT,
+    @BookId INT
 )
+AS
+BEGIN
+	INSERT INTO LoanDetails
+	VALUES(@Quantity, @LoanId, @BookId);
+END
+GO
+
+CREATE OR ALTER PROCEDURE spLoginCredentials_Update
+(
+    @DetailId INT,
+    @Quantity INT,
+    @LoanId INT,
+    @BookId INT
+)
+AS
+BEGIN
+    UPDATE LoanDetails
+    SET Quantity = @Quantity,
+        LoanId = @LoanId,
+        BookId = @BookId
+    WHERE DetailId = @DetailId;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE spLEmployees_GetAll
+AS
+BEGIN
+	SELECT BookId, Title, AuthorId, PublisherId, PublicationYear, Genre, Quantity
+	FROM Employees;
+END;
 GO
