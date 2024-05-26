268e1f2a19bebc9af4bf93b820c0d9208c43b667
keoto02 <guerrerodavid42069@gmail.com>
2024-05-25 20:12:28 -0600

    Modified by Carlos

--- MolinaTextileSystemDemo/MolinaTextileSystem/Models/ProductModel.cs
+++ MolinaTextileSystemDemo/MolinaTextileSystem/Models/ProductModel.cs
@@ -1,15 +1,26 @@
-﻿namespace MolinaTextileSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MolinaTextileSystem.Models
 {
     public class ProductModel
     {
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [DisplayName("Nombre de producto")]
         public string ProductName { get; set; }
 
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DisplayName("Tamaño de producto")]
         public string ProductSize { get; set; }
 
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DisplayName("Patrón")]
         public int PatternId { get; set; }
 
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DisplayName("Estado")]
         public int StateId { get; set; }
 
         public PatternModel Pattern { get; set; }
